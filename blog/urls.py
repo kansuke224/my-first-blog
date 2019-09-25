6eb7d9caada77c0420ef7b651d47a77c3aa806da
esaka <kansuke224224@gmail.com>
2019-09-25 16:32:17 +0900

    Modified templates to display posts from database.

---
+++ blog/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path # django の path 関数
+from . import views # ブログアプリのすべてのビューをインポート
+urlpatterns = [
+    path("", views.post_list, name="post_list")
+    # post_list という view をルートurl("/")に割り当てる
+    # name="post_list" は、viewを識別するために使われるURLの名前(?)
+]
--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.utils import timezone
+from .models import Post # . はカレントディレクトリにあることを示している
+# import するとき .py 拡張子は必要ない
 
 # Create your views here.
+def post_list(request): # request を引数にとる
+    # 変数postsはクエリセットを参照している
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by("published_date")
+    return render(request, "blog/post_list.html", {"posts": posts}) # render関数をreturn
+    # 名前と値をセットにしてテンプレートに渡したい引数を第三引数の map に記述する
