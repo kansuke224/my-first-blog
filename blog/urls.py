09838081761141788729e71b6d143863df6fdd7a
esaka <kansuke224224@gmail.com>
2019-09-28 11:00:44 +0900

    Added view and template for detailed blog post as well as CSS for the site.

--- blog/urls.py
+++ blog/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path # django の path 関数
 from . import views # ブログアプリのすべてのビューをインポート
 urlpatterns = [
-    path("", views.post_list, name="post_list")
+    path("", views.post_list, name="post_list"),
     # post_list という view をルートurl("/")に割り当てる
     # name="post_list" は、viewを識別するために使われるURLの名前(?)
+    path("post/<int:pk>", views.post_detail, name="post_detail")
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.utils import timezone
 from .models import Post # . はカレントディレクトリにあることを示している
 # import するとき .py 拡張子は必要ない
@@ -9,3 +9,7 @@ def post_list(request): # request を引数にとる
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by("published_date")
     return render(request, "blog/post_list.html", {"posts": posts}) # render関数をreturn
     # 名前と値をセットにしてテンプレートに渡したい引数を第三引数の map に記述する
+
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, "blog/post_detail.html", {"post": post})
