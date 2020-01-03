55b38e5a7c60317d0492113580abcf068eb6b203
esaka <kansuke224224@gmail.com>
2020-01-03 19:47:41 +0900

    gunicorn

--- mysite/settings.py
+++ mysite/settings.py
@@ -58,6 +58,7 @@ CLOUDINARY_STORAGE = {
 }
 
 MIDDLEWARE = [
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
